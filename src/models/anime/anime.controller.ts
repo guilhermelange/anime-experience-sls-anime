d0eca9ff227d5b7dd456e8e8032481b56d5eef33
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2023-10-06 22:35:20 -0300

    new: added docs upgrades

--- src/models/anime/anime.controller.ts
+++ src/models/anime/anime.controller.ts
@@ -10,16 +10,22 @@ import {
 import { Request } from 'express';
 import { AnimeService } from './anime.service';
 import { UpdateAnimeDto } from './dto/update-anime.dto';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
 @Controller('/')
+@ApiTags('Users  -  /users')
+@ApiSecurity('bearer')
 export class AnimeController {
   constructor(private readonly animeService: AnimeService) {}
+
   @Get()
+  @ApiOperation({ summary: 'Buscar todos os animes' })
   findAll(@Req() request: Request) {
     const { id: userId } = request.user;
     return this.animeService.findAll(userId);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Buscar anime por ID' })
   findOne(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Req() request: Request,
@@ -29,11 +35,13 @@ export class AnimeController {
   }
 
   @Get(':id/seasons')
+  @ApiOperation({ summary: 'Buscar temporadas por anime' })
   findOneSeasons(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.animeService.findOneSeasons(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Atualizar anime' })
   updateAnime(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Req() request: Request,
