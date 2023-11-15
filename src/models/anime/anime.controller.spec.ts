e8f3ae78b65d1f3c79d456370413a5530e6b3191
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2023-11-15 19:17:43 -0300

    new: added test files

--- src/models/anime/anime.controller.spec.ts
+++ src/models/anime/anime.controller.spec.ts
@@ -2,10 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaModule } from '../../common/database/prisma.module';
 import { AnimeController } from './anime.controller';
 import { AnimeService } from './anime.service';
+import { Request } from 'express';
+import { UpdateAnimeDto } from './dto/update-anime.dto';
 
 describe('AnimeController', () => {
   let controller: AnimeController;
   let service: AnimeService;
+  const mockRequest = { user: { id: '1' } } as Request;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,5 +24,64 @@ describe('AnimeController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of animes', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValueOnce([] as any[]);
+
+      const result = await controller.findAll(mockRequest);
+
+      expect(result.length).toEqual(0);
+      expect(service.findAll).toBeCalledTimes(1);
+      expect(service.findAll).toHaveBeenCalledWith(mockRequest.user.id);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return an anime', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValueOnce({} as any);
+
+      const result = await controller.findOne('1', mockRequest);
+
+      expect(result).toEqual({});
+      expect(service.findOne).toBeCalledTimes(1);
+      expect(service.findOne).toHaveBeenCalledWith('1', mockRequest.user.id);
+    });
+  });
+
+  describe('findOneSeasons', () => {
+    it('should return an anime seasons and episodes', async () => {
+      jest.spyOn(service, 'findOneSeasons').mockResolvedValueOnce([] as any[]);
+
+      const result = await controller.findOneSeasons('1');
+
+      expect(result).toEqual([]);
+      expect(service.findOneSeasons).toBeCalledTimes(1);
+      expect(service.findOneSeasons).toHaveBeenCalledWith('1');
+    });
+  });
+
+  describe('updateAnime', () => {
+    it('should update an anime', async () => {
+      jest.spyOn(service, 'updateOne').mockResolvedValueOnce('');
+
+      const updateAnimeDto = {
+        evaluation: true,
+      } as UpdateAnimeDto;
+      const result = await controller.updateAnime(
+        '1',
+        mockRequest,
+        updateAnimeDto,
+      );
+
+      expect(result).toEqual('');
+      expect(service.updateOne).toBeCalledTimes(1);
+      expect(service.updateOne).toHaveBeenCalledWith(
+        mockRequest.user.id,
+        updateAnimeDto,
+        '1',
+      );
+    });
   });
 });
--- src/models/anime/anime.service.spec.ts
+++ src/models/anime/anime.service.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaModule } from '../../common/database';
+import { PrismaModule, PrismaService } from '../../common/database';
 import { AnimeService } from './anime.service';
+import { UpdateAnimeDto } from './dto/update-anime.dto';
 
 describe('AnimeService', () => {
   let service: AnimeService;
+  let prisma: PrismaService;
+  let anime: any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +15,133 @@ describe('AnimeService', () => {
     }).compile();
 
     service = module.get<AnimeService>(AnimeService);
+    prisma = module.get<PrismaService>(PrismaService);
+
+    anime = {
+      id: '1',
+      name: 'Anime',
+      description: '',
+      cover_file: '',
+      parental_rating: 10,
+      start_date: new Date(),
+      image_file: '',
+      playlist_link: '',
+      anime_user_evaluation: [
+        {
+          evaluation: true,
+          userId: '1',
+          animeId: '1',
+        },
+      ],
+      anime_user_favorites: [],
+      season: [],
+    };
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(prisma).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a list of animes', async () => {
+      prisma.anime.findMany = jest.fn().mockResolvedValueOnce([anime] as any[]);
+
+      const response = await service.findAll('1');
+      expect(response[0].name).toEqual('Anime');
+      expect(prisma.anime.findMany).toBeCalledTimes(1);
+    });
+
+    it('should apply format', async () => {
+      const animeFormatted = service.loadAnimeResponse('1', anime);
+
+      expect(animeFormatted.name).toEqual('Anime');
+    });
+
+    it('should return a error', async () => {
+      prisma.anime.findMany = jest.fn().mockResolvedValueOnce([] as any[]);
+
+      expect(service.findAll('1')).rejects.toThrowError();
+      expect(prisma.anime.findMany).toBeCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return one anime', async () => {
+      prisma.anime.findUnique = jest.fn().mockResolvedValueOnce(anime as any);
+
+      const response = await service.findOne('1', '1');
+      expect(response.name).toEqual('Anime');
+      expect(prisma.anime.findUnique).toBeCalledTimes(1);
+    });
+
+    it('should return a error', async () => {
+      prisma.anime.findUnique = jest.fn().mockResolvedValueOnce(null);
+
+      expect(service.findOne('1', '0')).rejects.toThrowError();
+      expect(prisma.anime.findUnique).toBeCalledTimes(1);
+    });
+  });
+
+  describe('findOneSeasons', () => {
+    it('should return one anime seasons', async () => {
+      prisma.season.findMany = jest.fn().mockResolvedValueOnce([{}] as any[]);
+
+      const response = await service.findOneSeasons('1');
+      expect(response.length).toEqual(1);
+      expect(prisma.season.findMany).toBeCalledTimes(1);
+    });
+  });
+
+  describe('updateOne', () => {
+    it('should update an anime evaluation and favorite', async () => {
+      prisma.anime.findUnique = jest.fn().mockResolvedValueOnce(anime as any);
+      prisma.user.findUnique = jest
+        .fn()
+        .mockResolvedValueOnce({ id: '1' } as any);
+
+      prisma.animeUserFavorites.upsert = jest.fn().mockResolvedValueOnce('');
+      prisma.animeUserFavorites.delete = jest.fn().mockResolvedValueOnce('');
+      prisma.animeUserEvaluation.upsert = jest.fn().mockResolvedValueOnce('');
+
+      const animeUpdate = {
+        evaluation: true,
+        favorite: true,
+      } as UpdateAnimeDto;
+
+      const response = await service.updateOne('1', animeUpdate, '1');
+      expect(response).toEqual('');
+      expect(prisma.anime.findUnique).toBeCalledTimes(1);
+      expect(prisma.user.findUnique).toBeCalledTimes(1);
+      expect(prisma.animeUserEvaluation.upsert).toBeCalledTimes(1);
+      expect(prisma.animeUserFavorites.upsert).toBeCalledTimes(1);
+      expect(prisma.animeUserFavorites.delete).toBeCalledTimes(0);
+    });
+
+    it('should update an anime evaluation and favorite', async () => {
+      prisma.anime.findUnique = jest.fn().mockResolvedValueOnce(anime as any);
+      prisma.user.findUnique = jest
+        .fn()
+        .mockResolvedValueOnce({ id: '1' } as any);
+
+      prisma.animeUserFavorites.upsert = jest.fn().mockResolvedValueOnce('');
+      prisma.animeUserFavorites.delete = jest.fn().mockResolvedValueOnce('');
+      prisma.animeUserEvaluation.upsert = jest.fn().mockResolvedValueOnce('');
+
+      const animeUpdate = {
+        favorite: false,
+      } as UpdateAnimeDto;
+
+      const response = await service.updateOne('1', animeUpdate, '1');
+      expect(response).toEqual('');
+      expect(prisma.animeUserFavorites.delete).toBeCalledTimes(1);
+    });
+
+    // it('should return a error', async () => {
+    //   prisma.anime.findUnique = jest.fn().mockResolvedValueOnce(null);
+
+    //   expect(service.findOne('1', '0')).rejects.toThrowError();
+    //   expect(prisma.anime.findUnique).toBeCalledTimes(1);
+    // });
   });
 });
